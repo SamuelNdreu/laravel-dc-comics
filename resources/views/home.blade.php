639dca3520088cafdff7172fa3172f88c8541d89
SamuelNdreu <samwellsoficcial@gmail.com>
2023-10-02 01:00:28 +0200

    add button show, edit, cancel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,23 @@
 @include("partials.jumbo")
 <section id="serie-cards">
   <div class="container">
-    <div class="row pt-5">
-      @foreach ($comics as $comic)
-      <a href="{{ route('admin.show', $comic->id)}}" class="col-2 text-white text-decoration-none">
-        <img src="{{$comic->thumb}}" alt=" {{$comic->title}}" class="img-fluid">
+    <div class="row pt-5 align-items-stretch">
+        @foreach ($comics as $comic)
+        <div class="col-2 text-white pb-5 mb-4 position-relative">
+            <img src="{{$comic->thumb}}" alt=" {{$comic->title}}" class="img-fluid">
         <div>
           {{$comic->series}}
         </div>
       </a>
+      <div class="position-absolute bottom-0">
+        <a href="{{ route('admin.show', $comic->id)}}" class="btn btn-primary btn-sm">Show</a>
+        <a href="" class="btn btn-warning btn-sm">Edit</a>
+        <a href="" class="btn btn-danger btn-sm">Cancel</a>
+      </div>
+    </div>
       @endforeach
     </div>
-    
+
     <div class="text-center">
       <a href="{{route('admin.create')}}" class="btn btn-primary my-4">ADD</a> 
     </div>
