67f0f6a4c58f4425eefaa70005bfd7c1a9e374b0
SamuelNdreu <samwellsoficcial@gmail.com>
2023-10-02 01:04:37 +0200

    update data db

--- app/Http/Controllers/ComicProductController.php
+++ app/Http/Controllers/ComicProductController.php
@@ -51,7 +51,7 @@ class ComicProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Comic $comic
      * @return \Illuminate\Http\Response
      */
     public function show(Comic $comic)
@@ -64,24 +64,27 @@ class ComicProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Comic $comic
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Comic $comic)
     {
-        //
+        return view("edit", compact("comic"));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Comic $comic
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comic $comic)
     {
-        //
+        $data= $request->all();
+        $comic->update($data);
+
+        return redirect()->route("admin.show", $comic->id);
     }
 
     /**
